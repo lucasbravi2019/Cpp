2b2310560e3e7812f43ddb8bc56c350ec7b9e1bb
Lucas <lucas.bravi.2008@gmail.com>
2021-08-30 12:11:44 -0300

    Ejercicio Algoritmos

--- EjercicioGrupoAlgoritmos/Notas.cpp
+++ EjercicioGrupoAlgoritmos/Notas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 double promedioNotas(double nota1, double nota2, double nota3)
@@ -16,42 +17,49 @@ double promedioGlobales(double promedio)
     return promedio * .6;
 }
 
+double notasCambiadasParcial(double nota, int numero)
+{
+    cout << "Ingrese la nota " << numero << " del parcial\n";
+    cin >> nota;
+    return nota;
+}
+
+double notasCambiadasGlobal(double nota, int numero)
+{
+    cout << "Ingrese la nota " << numero << " del global\n";
+    cin >> nota;
+    return nota;
+}
+
+string promediar(double promedio)
+{
+    if (promedio >= 60)
+    {
+        return "Promocionado\n";
+    }
+    if (promedio >= 40 && promedio < 60)
+    {
+        return "Regular\n";
+    }
+    if (promedio < 40)
+    {
+        return "Libre\n";
+    }
+}
+
 void notas()
 {
-    double nota1P;
-    double nota2P;
-    double nota3P;
-    double nota1G;
-    double nota2G;
-    double nota3G;
-    cout << "Ingrese la nota 1 del parcial\n";
-    cin >> nota1P;
-    cout << "Ingrese la nota 2 del parcial\n";
-    cin >> nota2P;
-    cout << "Ingrese la nota 3 del parcial\n";
-    cin >> nota3P;
-    cout << "Ingrese la nota 1 del global\n";
-    cin >> nota1G;
-    cout << "Ingrese la nota 2 del global\n";
-    cin >> nota2G;
-    cout << "Ingrese la nota 3 del global\n";
-    cin >> nota3G;
+    double nota1P = notasCambiadasParcial(nota1P, 1);
+    double nota2P = notasCambiadasParcial(nota2P, 2);
+    double nota3P = notasCambiadasParcial(nota3P, 3);
+    double nota1G = notasCambiadasGlobal(nota1G, 1);
+    double nota2G = notasCambiadasGlobal(nota2G, 2);
+    double nota3G = notasCambiadasGlobal(nota3G, 3);
     double parciales = promedioNotas(nota1P, nota2P, nota3P);
     double globales = promedioNotas(nota1G, nota2G, nota3G);
     double promedio = promedioParciales(parciales) + promedioGlobales(globales);
     cout << "El promedio es " << promedio << "\n";
-    if (promedio >= 60)
-    {
-        cout << "Promocionado\n";
-    }
-    if (promedio >= 40 && promedio < 60)
-    {
-        cout << "Regular\n";
-    }
-    if (promedio < 40)
-    {
-        cout << "Libre\n";
-    }
+    cout << promediar(promedio);
 }
 
 int main(int argc, char const *argv[])
