8dcdd8a2033272518284f15b56e6f162da477ebd
Lucas <lucas.bravi.2008@gmail.com>
2021-09-20 16:45:18 -0300

    Ejercicios C++

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int sumar(int numero)
+int fibo(int numero)
 {
     if (numero < 3 && numero > 0)
     {
@@ -16,13 +16,89 @@ int sumar(int numero)
     }
     else
     {
-        return sumar(numero - 1) + sumar(numero - 2);
+        return fibo(numero - 1) + fibo(numero - 2);
+    }
+}
+
+int sumar(int numero)
+{
+    if (numero == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return numero + sumar(numero - 1);
+    }
+}
+
+int restar(int numero)
+{
+    if (numero == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return -numero + restar(numero - 1);
+    }
+}
+
+int multiplicar(int numero)
+{
+    if (numero == 1)
+    {
+        return numero;
+    }
+    else
+    {
+        return numero * multiplicar(numero - 1);
+    }
+}
+
+float division(int numero, int dividendo)
+{
+    if (numero < dividendo)
+    {
+        return numero;
+    }
+    else
+    {
+        return division(numero - dividendo, dividendo);
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    cout << sumar(5);
+    cout << "Fibonacci\n";
+    for (int i = 1; i < 10; i++)
+    {
+        cout << fibo(i) << "\n";
+    }
+
+    cout << "Suma\n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << sumar(i) << "\n";
+    }
+
+    cout << "Resta\n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << restar(i) << "\n";
+    }
+
+    cout << "Multiplicacion\n";
+    for (int i = 1; i < 10; i++)
+    {
+        cout << multiplicar(i) << "\n";
+    }
+
+    cout << "Division\n";
+    for (int i = 1; i < 10; i++)
+    {
+        cout << division(i, 4) << "\n";
+    }
 
     return 0;
 }
